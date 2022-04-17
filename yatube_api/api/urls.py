0891db0644616f0d6b6b7f11e68c9e9509de0c2f
Aleksandr <haus.esc@gmail.com>
2022-04-18 01:38:10 +0300

    18/04

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 
 rout = DefaultRouter()
 rout.register(r'posts/(?P<id>\d+)/comments',
-                CommentViewSet, basename='comments')
+              CommentViewSet, basename='comments')
 rout.register(r'groups', GroupViewSet, basename='groups')
 rout.register(r'posts', PostViewSet, basename='posts')
 
