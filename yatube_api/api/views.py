ba69c2d21307a96717ba8dfaba1588e11a903040
Aleksandr <haus.esc@gmail.com>
2022-04-19 08:21:58 +0300

    19/04

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404
-
 from rest_framework import viewsets, permissions
+
 from api.permission import IsAuthorOrReadOnly
 from api.serializers import PostSerializer, CommentSerializer, GroupSerializer
-
 from posts.models import Post, Group
 
 
@@ -18,7 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthorOrReadOnly, permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs['id'])
